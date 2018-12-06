6812637ab31231b49147cafada26d6cfa9bcf487
elrohil <w.stanek123@gmail.com>
2018-12-06 17:57:45 +0100

    Added non-interactive option

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,4 +4,4 @@ git clone git@js-embedded-platform:Elrohil44/js-embedded-platform-cli cli
 
 rm -rf cli/dist && cp dist cli/dist -r
 
-cd cli && git add dist && yarn version --patch && git push
+cd cli && git add dist && yarn version --patch --non-interactive && git push
