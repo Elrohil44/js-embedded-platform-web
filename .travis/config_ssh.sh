14ff31de6fb4fbb6ad8618ac181dbc169a6ed282
elrohil <w.stanek123@gmail.com>
2018-12-06 17:41:43 +0100

    Added version bumping

--- .travis/config_ssh.sh
+++ .travis/config_ssh.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-echo " `pwd`/.travis/deploy_key" >> .travis/.ssh_config
-echo >> ~/.ssh/config
-cat .travis/.ssh_config >> ~/.ssh/config
+echo "IdentityFile `pwd`/.travis/deploy_key" >> .travis/.ssh_config
+echo >> ~/.ssh/config && cat .travis/.ssh_config >> ~/.ssh/config
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,11 +2,6 @@
 
 git clone git@js-embedded-platform:Elrohil44/js-embedded-platform-cli cli
 
-rm -rf cli/dist
-cp dist cli/dist -r
+rm -rf cli/dist && cp dist cli/dist -r
 
-cd cli
-git add dist
-git commit -m "Deployed new web build"
-
-git push
+cd cli && git add dist && yarn version --patch && git push
