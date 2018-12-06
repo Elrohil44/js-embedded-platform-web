1bc7e6fd0301b54a06db2487c2c9a38094b72836
elrohil <w.stanek123@gmail.com>
2018-12-06 17:46:22 +0100

    Added permissions of private key changing

--- .travis/config_ssh.sh
+++ .travis/config_ssh.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
+chmod 400 ./travis/deploy_key
 echo "IdentityFile `pwd`/.travis/deploy_key" >> .travis/.ssh_config
 echo >> ~/.ssh/config && cat .travis/.ssh_config >> ~/.ssh/config
